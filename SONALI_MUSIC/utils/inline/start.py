df48b292443280a0b8817484db344f429df2d3c6
⎯𓆩꯭ 𝐀 ꯭ʟ ꯭ᴘ ꯭ʜ꯭ ᴧ꯭🥂꯭༎꯭ 𓆪꯭ <168528642+TEAMPURVI@users.noreply.github.com>
2025-01-13 20:22:50 +0530

    ᴛᴇᴀᴍ ᴘᴜʀᴠɪ ʙᴏᴛs

--- SONALI_MUSIC/utils/inline/start.py
+++ SONALI_MUSIC/utils/inline/start.py
@@ -19,22 +19,17 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_3"],url=f"https://t.me/{app.username}?startgroup=true",)
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_CP"),
-        ],
-        
-        [
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="ALLBOT_CP"),
-            InlineKeyboardButton(text=_["S_B_11"], callback_data="PROMOTION_CP"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_CP"),
         ],
     ]
     return buttons
