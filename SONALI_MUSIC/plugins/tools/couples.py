6b3c3bc298aa3b42a6f14736fe5b744f63f66221
⎯𓆩꯭ 𝐀 ꯭ʟ ꯭ᴘ ꯭ʜ꯭ ᴧ꯭🥂꯭༎꯭ 𓆪꯭ <168528642+TEAMPURVI@users.noreply.github.com>
2025-01-15 17:26:07 +0530

    ᴛᴇᴀᴍ ᴘᴜʀᴠɪ ʙᴏᴛs

--- SONALI_MUSIC/plugins/tools/couples.py
+++ SONALI_MUSIC/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •",
-            url=f"https://t.me/ThunderMusicRobot?startgroup=true",
+            url=f"https://t.me/Sonali_Music_bot?startgroup=true",
         ),
     ],
 ]
