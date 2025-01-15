715b9019ad478793de9c1c669de079d1a14b6851
⎯𓆩꯭ 𝐀 ꯭ʟ ꯭ᴘ ꯭ʜ꯭ ᴧ꯭🥂꯭༎꯭ 𓆪꯭ <168528642+TEAMPURVI@users.noreply.github.com>
2025-01-15 22:46:37 +0530

    ᴛᴇᴀᴍ ᴘᴜʀᴠɪ ʙᴏᴛs

--- SONALI_MUSIC/plugins/mics/gayfun.py
+++ SONALI_MUSIC/plugins/mics/gayfun.py
@@ -31,26 +31,3 @@ def gay_calculator_command(client, message: Message):
 
     # Send the gay response as a message
     message.reply_text(f"Gay Percentage: {gay_percentage}%\n{gay_response}")
-
-
-
-
-@app.on_message(filters.command("logo"))
-async def logo(app, msg: Message):
-    if len(msg.command) == 1:
-       return await msg.reply_text("Usage:\n\n /logo Sona")
-    logo_name = msg.text.split(" ", 1)[1]
-    API = f"https://api.sdbots.tech/logohq?text={logo_name}"
-    req = requests.get(API).url
-    await msg.reply_photo(
-        photo=f"{req}")
-
-@app.on_message(filters.command("animelogo"))
-async def logo(app, msg: Message):
-    if len(msg.command) == 1:
-       return await msg.reply_text("Usage:\n\n /animelogo Sona")
-    logo_name = msg.text.split(" ", 1)[1]
-    API = f"https://api.sdbots.tech/anime-logo?name={logo_name}"
-    req = requests.get(API).url
-    await msg.reply_photo(
-        photo=f"{req}")
