9f31924f7c3cf0fb17c587a6a87ccb20afc71c8f
⎯𓆩꯭ 𝐀 ꯭ʟ ꯭ᴘ ꯭ʜ꯭ ᴧ꯭🥂꯭༎꯭ 𓆪꯭ <168528642+TEAMPURVI@users.noreply.github.com>
2025-01-15 20:49:56 +0530

    Update start.py

--- SONALI_MUSIC/utils/inline/start.py
+++ SONALI_MUSIC/utils/inline/start.py
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["˹ ᴧʙσυт ˼"], callback_data="ALLBOT_CP"),
+            InlineKeyboardButton("˹ ᴧʙσυт ˼", callback_data="ALLBOT_CP"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_CP"),
