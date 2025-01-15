3a242a481900cb4e6f6b04ceced92a76f118d542
RAUSHAN-SONALI-143 <156349324+RAUSHAN-SONALI-143@users.noreply.github.com>
2025-01-16 00:55:25 +0530

    Update waifu.py

--- SONALI_MUSIC/plugins/sudo/waifu.py
+++ SONALI_MUSIC/plugins/sudo/waifu.py
@@ -1,39 +1,37 @@
 from pyrogram import Client, filters
-import httpx
+import requests
 from SONALI_MUSIC import app
 
 waifu_api_url = 'https://api.waifu.im/search'
 
+# SANATANI
 
-async def get_waifu_data(tags):
+def get_waifu_data(tags):
     params = {
         'included_tags': tags,
         'height': '>=2000'
     }
-    async with httpx.AsyncClient() as client:
-        response = await client.get(waifu_api_url, params=params)
+
+    response = requests.get(waifu_api_url, params=params)
 
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
-
 @app.on_message(filters.command("waifu"))
-async def waifu_command(client, message):
+def waifu_command(client, message):
     try:
-        # Extract tags from the message
-        command, *args = message.text.split(" ")
-        tags = args if args else ['maid']  # Default to 'maid' if no tags are provided
+        tags = ['maid']  # You can customize the tags as needed
+        waifu_data = get_waifu_data(tags)
 
-        waifu_data = await get_waifu_data(tags)
-
-        if waifu_data and 'images' in waifu_data and len(waifu_data['images']) > 0:
+        if waifu_data and 'images' in waifu_data:
             first_image = waifu_data['images'][0]
             image_url = first_image['url']
-            await message.reply_photo(image_url)
+            message.reply_photo(image_url)
         else:
-            await message.reply_text("❍ ɴᴏ ᴡᴀɪғᴜ ғᴏᴜɴᴅ ғᴏʀ ᴛʜᴇ sᴘᴇᴄɪғɪᴇᴅ ᴛᴀɢs.")
+            message.reply_text("No waifu found with the specified tags.")
+
     except Exception as e:
-        await message.reply_text(f"❍ ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ: {str(e)}")
+        message.reply_text(f"An error occurred: {str(e)}")
         
