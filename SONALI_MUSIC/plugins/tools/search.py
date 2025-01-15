7ed58e45d1f76f0be98af955845a9b3dee29081f
⎯𓆩꯭ 𝐀 ꯭ʟ ꯭ᴘ ꯭ʜ꯭ ᴧ꯭🥂꯭༎꯭ 𓆪꯭ <168528642+TEAMPURVI@users.noreply.github.com>
2025-01-15 19:22:31 +0530

    ᴛᴇᴀᴍ ᴘᴜʀᴠɪ ʙᴏᴛs

--- SONALI_MUSIC/plugins/tools/search.py
+++ SONALI_MUSIC/plugins/tools/search.py
@@ -1,197 +1,37 @@
-from traceback import format_exc
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from search_engine_parser.core.engines.google import Search as GoogleSearch
-from search_engine_parser.core.engines.stackoverflow import \
-    Search as StackSearch
-from search_engine_parser.core.exceptions import NoResultsFound, NoResultsOrTrafficError
-from SONALI_MUSIC import app
+import logging
+
+from googlesearch import search
 from pyrogram import filters
 
+from SONALI_MUSIC import app
+from SafoneAPI import SafoneAPI
 
 
+@app.on_message(filters.command(["google", "gle"]))
+async def google(bot, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        await message.reply_text("Example:\n\n/google lord ram")
+        return
 
-gsearch = GoogleSearch()
-stsearch = StackSearch()
-
-
-
-def ikb(rows=None, back=False, todo="start_back"):
-    """
-    rows = pass the rows
-    back - if want to make back button
-    todo - callback data of back button
-    """
-    if rows is None:
-        rows = []
-    lines = []
+    if message.reply_to_message and message.reply_to_message.text:
+        user_input = message.reply_to_message.text
+    else:
+        user_input = " ".join(message.command[1:])
+    b = await message.reply_text("Sᴇᴀʀᴄʜɪɴɢ ᴏɴ Gᴏᴏɢʟᴇ....")
     try:
-        for row in rows:
-            line = []
-            for button in row:
-                btn_text = button.split(".")[1].capitalize()
-                button = btn(btn_text, button)  
-                line.append(button)
-            lines.append(line)
-    except AttributeError:
-        for row in rows:
-            line = []
-            for button in row:
-                button = btn(*button)  
-                line.append(button)
-            lines.append(line)
-    except TypeError:
-        # make a code to handel that error
-        line = []
-        for button in rows:
-            button = btn(*button)  # InlineKeyboardButton
-            line.append(button)
-        lines.append(line)
-    if back: 
-        back_btn = [(btn("ʙᴀᴄᴋ", todo))]
-        lines.append(back_btn)
-    return InlineKeyboardMarkup(inline_keyboard=lines)
-
-
-def btn(text, value, type="callback_data"):
-    return InlineKeyboardButton(text, **{type: value})
-
-
-
-
-
-
-@app.on_message(filters.command('google'))
-async def search_(app: app, msg: Message):
-    split = msg.text.split(None, 1)
-    if len(split) == 1:
-        return await msg.reply_text("**ɢɪᴠᴇ ǫᴜᴇʀʏ ᴛᴏ sᴇᴀʀᴄʜ**")
-    to_del = await msg.reply_text("**sᴇᴀʀᴄʜɪɴɢ ᴏɴ ɢᴏᴏɢʟᴇ...**")
-    query = split[1]
-    try:
-        result = await gsearch.async_search(query)
-        keyboard = ikb(
-            [
-                [
-                    (
-                        f"{result[0]['titles']}",
-                        f"{result[0]['links']}",
-                        "url",
-                    ),
-                ],
-                [
-                    (
-                        f"{result[1]['titles']}",
-                        f"{result[1]['links']}",
-                        "url",
-                    ),
-                ],
-                [
-                    (
-                        f"{result[2]['titles']}",
-                        f"{result[2]['links']}",
-                        "url",
-                    ),
-                ],
-                [
-                    (
-                        f"{result[3]['titles']}",
-                        f"{result[3]['links']}",
-                        "url",
-                    ),
-                ],
-                [
-                    (
-                        f"{result[4]['titles']}",
-                        f"{result[4]['links']}",
-                        "url",
-                    ),
-                ],
-            ]
+        a = search(user_input, advanced=True)
+        txt = f"Search Query: {user_input}\n\nresults"
+        for result in a:
+            txt += f"\n\n[❍ {result.title}]({result.url})\n<b>{result.description}</b>"
+        await b.edit(
+            txt,
+            disable_web_page_preview=True,
         )
-
-        txt = f"**ʜᴇʀᴇ ᴀʀᴇ ᴛʜᴇ ʀᴇsᴜʟᴛs ᴏғ ʀǫᴜᴇsᴛᴇᴅ : {query.title()}**"
-        await to_del.delete()
-        await msg.reply_text(txt, reply_markup=keyboard)
-        return
-    except NoResultsFound:
-        await to_del.delete()
-        await msg.reply_text("**ɴᴏ ʀᴇsᴜʟᴛ ғᴏᴜɴᴅ ᴄᴏʀʀᴇsᴘᴏɴᴅɪɴɢ ᴛᴏ ʏᴏᴜʀ ǫᴜᴇʀʏ**")
-        return
-    except NoResultsOrTrafficError:
-        await to_del.delete()
-        await msg.reply_text("****ɴᴏ ʀᴇsᴜʟᴛ ғᴏᴜɴᴅ ᴅᴜᴇ ᴛᴏ ᴛᴏᴏ ᴍᴀɴʏ ᴛʀᴀғғɪᴄ**")
-        return
     except Exception as e:
-        await to_del.delete()
-        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @iam_Sona")
-        print(f"error : {e}")
-        return
+        await b.edit(e)
+        logging.exception(e)
 
 
+__MODULE__ = "Gᴏᴏɢʟᴇ"
+__HELP__ = """/google [ǫᴜᴇʀʏ] - ᴛᴏ sᴇᴀʀᴄʜ ᴏɴ ɢᴏᴏɢʟᴇ ᴀɴᴅ ɢᴇᴛ ʀᴇsᴜʟᴛs"""
 
-@app.on_message(filters.command('stack'))
-async def stack_search_(app: app, msg: Message):
-    split = msg.text.split(None, 1)
-    if len(split) == 1:
-        return await msg.reply_text("**ɢɪᴠᴇ ǫᴜᴇʀʏ ᴛᴏ sᴇᴀʀᴄʜ**")
-    to_del = await msg.reply_text("**sᴇᴀʀᴄʜɪɴɢ ᴏɴ ɢᴏᴏɢʟᴇ...**")
-    query = split[1]
-    try:
-        result = await stsearch.async_search(query)
-        keyboard = ikb(
-            [
-                [
-                    (
-                        f"{result[0]['titles']}",
-                        f"{result[0]['links']}",
-                        "url",
-                    ),
-                ],
-                [
-                    (
-                        f"{result[1]['titles']}",
-                        f"{result[1]['links']}",
-                        "url",
-                    ),
-                ],
-                [
-                    (
-                        f"{result[2]['titles']}",
-                        f"{result[2]['links']}",
-                        "url",
-                    ),
-                ],
-                [
-                    (
-                        f"{result[3]['titles']}",
-                        f"{result[3]['links']}",
-                        "url",
-                    ),
-                ],
-                [
-                    (
-                        f"{result[4]['titles']}",
-                        f"{result[4]['links']}",
-                        "url",
-                    ),
-                ],
-            ]
-        )
-
-        txt = f"**ʜᴇʀᴇ ᴀʀᴇ ᴛʜᴇ ʀᴇsᴜʟᴛs ᴏғ ʀǫᴜᴇsᴛᴇᴅ : {query.title()}**"
-        await to_del.delete()
-        await msg.reply_text(txt, reply_markup=keyboard)
-        return
-    except NoResultsFound:
-        await to_del.delete()
-        await msg.reply_text("**ɴᴏ ʀᴇsᴜʟᴛ ғᴏᴜɴᴅ ᴄᴏʀʀᴇsᴘᴏɴᴅɪɴɢ ᴛᴏ ʏᴏᴜʀ ǫᴜᴇʀʏ**")
-        return
-    except NoResultsOrTrafficError:
-        await to_del.delete()
-        await msg.reply_text("****ɴᴏ ʀᴇsᴜʟᴛ ғᴏᴜɴᴅ ᴅᴜᴇ ᴛᴏ ᴛᴏᴏ ᴍᴀɴʏ ᴛʀᴀғғɪᴄ**")
-        return
-    except Exception as e:
-        await to_del.delete()
-        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @DevsOops")
-        print(f"error : {e}")
-        return
