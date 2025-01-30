2ee42ca3e85c0324eeecefdf7a504986be90f0ad
RAUSHAN-SONALI-143 <156349324+RAUSHAN-SONALI-143@users.noreply.github.com>
2025-01-31 03:00:25 +0530

    ᴛᴇᴀᴍ ᴘᴜʀᴠɪ ʙᴏᴛs

--- SONALI_MUSIC/utils/thumbnails.py
+++ SONALI_MUSIC/utils/thumbnails.py
@@ -86,8 +86,8 @@ async def get_thumb(videoid):
         draw.text((565, 180), title1[0], fill=(255, 255, 255), font=title_font)
         draw.text((565, 230), title1[1], fill=(255, 255, 255), font=title_font)
         draw.text((565, 320), f"{channel}  |  {views[:23]}", (255, 255, 255), font=arial)
-        text_size = draw.textsize("TEAM PURVI BOTS    ", font=font)
-        draw.text((1280 - text_size[0] - 10, 10), "TEAM PURVI BOTS    ", fill="yellow", font=font)
+        text_size = draw.textsize("TEAM PURVI BOTS   ", font=font)
+        draw.text((1280 - text_size[0] - 10, 10), "TEAM PURVI BOTS   ", fill="yellow", font=font)
         line_length = 580
         red_length = int(line_length * 0.6)
         draw.line([(565, 380), (565 + red_length, 380)], fill="red", width=9)
