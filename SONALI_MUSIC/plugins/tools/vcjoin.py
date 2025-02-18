b2562489a1d415f4ce01ff38c5814b573262fa54
RAUSHAN-SONALI-143 <156349324+RAUSHAN-SONALI-143@users.noreply.github.com>
2025-02-18 18:09:00 +0530

    Update vcjoin.py

--- SONALI_MUSIC/plugins/tools/vcjoin.py
+++ SONALI_MUSIC/plugins/tools/vcjoin.py
@@ -3,16 +3,22 @@ from pyrogram.types import ChatPrivileges
 from SONALI_MUSIC import app
 
 
+import asyncio
+
+
 @app.on_chat_member_updated()
 async def video_chat_event(client, update):
-    if update.new_chat_member and update.new_chat_member.privileges:
-        if update.new_chat_member.privileges.can_manage_video_chats:
-            user = update.new_chat_member.user
-            chat_id = update.chat.id
-            mention = f"[{user.first_name}](tg://user?id={user.id})"
-            username = f"@{user.username}" if user.username else "ηᴏ ᴜsєʀηᴧϻє ☹️"
-            user_id = user.id
+    if update.new_chat_member:
+        user = update.new_chat_member.user
+        chat_id = update.chat.id
+        mention = f"[{user.first_name}](tg://user?id={user.id})"
+        username = f"@{user.username}" if user.username else "No Username"
+        user_id = user.id
 
-            message = f"**⚘ ᴊσɪηєᴅ ᴠɪᴅєᴏ ᴄʜᴧᴛ**\n\n**:⧽ ηᴧϻє :** {mention}\n**:⧽ ᴜsєʀ ɪᴅ :** `{user_id}`\n**:⧽ ᴜsєʀηᴧϻє :** {username}"
-            
-            await client.send_message(chat_id, message)
+        message = f"**⚘ ᴊσɪηєᴅ ᴠɪᴅєᴏ ᴄʜᴧᴛ**\n\n**:⧽ ηᴧϻє :** {mention}\n**:⧽ ᴜsєʀ ɪᴅ :** `{user_id}`\n**:⧽ ᴜsєʀηᴧϻє :** {username}"
+        
+        msg = await client.send_message(chat_id, message)  # मैसेज भेजें
+        await asyncio.sleep(60)  # 1 मिनट बाद मैसेज डिलीट करें
+        await client.delete_messages(chat_id, msg.message_id)  # मैसेज डिलीट करें
+
+app.run()
