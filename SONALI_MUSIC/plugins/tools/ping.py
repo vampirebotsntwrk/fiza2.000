d221adec1f919989a8e7c2e2d1eb01e823e6b7e5
BADNAM XD <yadavxd746@gmail.com>
2025-04-27 18:30:10 +0530

    Update ping.py

--- SONALI_MUSIC/plugins/tools/ping.py
+++ SONALI_MUSIC/plugins/tools/ping.py
@@ -71,10 +71,10 @@ async def ping_com(client, message: Message, _):
         ],
         [
             InlineKeyboardButton(
-                text="• ɢʀᴏᴜᴘ •", url=f"https://t.me/+gMy8Cp190ediNzZl",
+                text="• ɢʀᴏᴜᴘ •", url=f"https://t.me/TEAM_BADNAM_BOTS",
             ),
             InlineKeyboardButton(
-                text="• ᴍᴏʀᴇ •", url=f"https://t.me/PURVI_SUPPORT",
+                text="• ᴍᴏʀᴇ •", url=f"https://t.me/KRITI_UPDATE",
             )
         ],
         [
