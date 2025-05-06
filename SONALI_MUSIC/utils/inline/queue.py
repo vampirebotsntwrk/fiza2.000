d401f6fc99bdeffaab7a9e749f923b8c72378f00
BADNAM XD <yadavxd746@gmail.com>
2025-05-07 03:54:05 +0530

    Update queue.py

--- SONALI_MUSIC/utils/inline/queue.py
+++ SONALI_MUSIC/utils/inline/queue.py
@@ -78,7 +78,7 @@ def aq_markup(_, chat_id):
                 text="˹❍ᴡηєʀ ᴀʙσᴜᴛ˼ ", url=f"https://t.me/KRITI_UPDATE"
             ),
             InlineKeyboardButton(
-                text="˹ᴄʜᴧᴛ ɢʀσᴜᴘ˼", url=f"https://t.me/TEAM_BADNAM_BOTS"
+                text="˹ᴄʜᴧᴛ ɢʀσᴜᴘ˼", url=f"https://t.me/KRITI_SUPPORT_CHAT"
             ),
         ],
       ]
