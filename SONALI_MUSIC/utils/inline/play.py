df30ec3a7ec0f2b7e892ae46a5cb544ce44b3105
BADNAM XD <yadavxd746@gmail.com>
2025-07-29 09:36:44 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -64,8 +64,8 @@ def stream_markup_timer(_, chat_id, played, dur):
          InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-         InlineKeyboardButton(text="• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/KRITI_UPDATE"),
-         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/KRITI_SUPPORT_CHAT"),
+         InlineKeyboardButton(text="• υρԃαƚҽ •", url="https://t.me/About_badnam_xd"),
+         InlineKeyboardButton(text="• ɠɾσυρ •", url="https://t.me/+G-kGeNJKv_YxNTg9"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -75,8 +75,8 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/KRITI_UPDATE"),
-         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/KRITI_SUPPORT_CHAT"),
+         InlineKeyboardButton(text="• υρ∂тє •", url="https://t.me/About_badnam_xd"),
+         InlineKeyboardButton(text="• gяσυρ •", url="https://t.me/+G-kGeNJKv_YxNTg9"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
